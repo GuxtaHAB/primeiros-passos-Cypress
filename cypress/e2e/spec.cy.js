1089d0b10ef5d692ee6439f53dc218f4d4718e22
sarah aguiar <gustavoberalto05@gmail.comgit config --global user.name Gustavo>
2025-03-18 15:59:49 -0300

    First COmmit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,20 @@
+describe('Login - Success', () => {
+  it('passes', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type("Admin")
+    cy.get("[name='password']").type("admin123")
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+})
+
+describe('Login - Fail', () => {
+  it('passes', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type("Test")
+    cy.get("[name='password']").type("fail123")
+    cy.get("[type='submit']").click()
+    cy.get("[role='alert']")
+  })
+})
